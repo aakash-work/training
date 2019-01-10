2c247a89878518112485b190f44922422b22454f
kavya <kavya.sai@miqdigital.com>
2019-01-10 18:34:10 +0530

    commit

---
+++ assignment1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo 'Enter required age'
+read input
+
+mysql -u root -p -e"
+USE db2;
+CREATE TABLE IF NOT EXISTS \`voters\` (
+  \`id\` int(11) NOT NULL AUTO_INCREMENT,
+  \`email\` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (\`id\`)
+);
+
+INSERT INTO voters  
+   (id,email)  
+SELECT id,email 
+FROM authors 
+WHERE DATEDIFF(CURDATE(),birthdate)/365.25>=$input and authors.id NOT IN (select id from voters);
+ 
+
+DROP TABLE IF EXISTS \`newvoters\`;
+
+CREATE TABLE \`newvoters\` (
+  \`id\` int(11) NOT NULL AUTO_INCREMENT,
+  \`email\` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (\`id\`)
+);
+
+INSERT INTO newvoters  
+   (id,email)
+SELECT id,email 
+FROM authors 
+WHERE DATEDIFF(CURDATE(),birthdate)/365.25>=$input; 
+
+INSERT INTO voters  
+   (id,email)
+SELECT id,email 
+FROM newvoters;
+
+select * from newvoters;" >a.txt
