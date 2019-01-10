712abafa2fa7676f8359d170ae424b8de4ddec9f
Ayushman Dey <ayushman.dey@miqdigital.com>
2019-01-10 17:56:30 +0530

    Storing data based on required specs

--- day1.sh
+++ day1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -n "enter valid age: "
 read age
-sudo mysql -e "use author; select id, first_name, last_name from authors where abs(datediff(birthdate,curdate())/365.25) >= "$age";" > answer.txt
+sudo mysql -e "use author; select id, email from authors where abs(datediff(birthdate,curdate())/365.25) >= "$age";" > answer.txt
 uniq -u answer.txt;
