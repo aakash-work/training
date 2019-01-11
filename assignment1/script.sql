80e3d6afdfd29f1ec81e108ead281d77efaa9d4c
Siddhanth.vinay <sidvin97@gmail.com>
2019-01-11 14:40:39 +0530

    Made a correction

--- assignment1/script.sql
+++ assignment1/script.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS added(
 select id,email from authors where (year(curdate())-year(birthdate) > @age) and id not in (select eligible_id from added);
 
 
-insert into added select id,email from authors where (year(curdate())-year(birthdate) >18) and id not in (select eligible_id from added);
+insert into added select id,email from authors where (year(curdate())-year(birthdate) >@age) and id not in (select eligible_id from added);
 
 
