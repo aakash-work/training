43db72136b93289c85174c2a49b78984f261a23d
ayushkul2910 <kulshrestha.ayush2910@gmail.com>
2019-01-16 21:43:23 +0530

    Assignment1

---
+++ Assignment1/test3.sh
@@ -0,0 +1,14 @@
+
+#create a table timeStmp with a column time as timestamp and add value '1970-01-01 10:10:10'
+
+#!/bin/bash
+
+read input
+output=$(sudo mysql Test <<EOF
+use Test
+select id, email from authors where abs(datediff(birthdate,curdate())/365)>="$input" and added>(select * from timeStmp) ;
+truncate timeStmp;
+insert into timeStmp values(now());
+EOF
+)
+echo "$output" >> outfile
